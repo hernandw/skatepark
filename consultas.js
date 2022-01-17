a08522a1e9f98a11609ae2ef560a288f15dec14a
Williams Hernandez <hernandw@gmail.com>
2022-01-17 11:29:03 -0300

    ajustes del proyecto

---
+++ consultas.js
@@ -0,0 +1,157 @@
+const { Pool } = require("pg");
+const bcrypt = require('bcrypt');
+require('dotenv').config({ path: 'variables.env' })
+// configuracion de la instancia Pool
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+
+
+// Función asincrónica para consultar todos los usuarios
+const getUsuarios = async () => {
+  try {
+    const usuarios = await pool.query("SELECT * FROM skaters");
+    return usuarios.rows;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Función asincrónica para ingresar un usuario
+const nuevoUsuario = async (
+  email,
+  nombre,
+  password,
+  anos_experiencia,
+  especialidad,
+  foto
+) => {
+  let passwordHash = await hashPassword(password);
+  let params = {
+    text: `INSERT INTO skaters 
+(email,nombre,password,anos_experiencia,especialidad,foto,estado)
+VALUES ($1, $2, $3, $4, $5, $6, $7)
+RETURNING *`,
+    values: [
+      email,
+      nombre,
+      passwordHash,
+      anos_experiencia,
+      especialidad,
+      foto,
+      false,
+    ],
+  };
+  try {
+    const result = await pool.query(params);
+    
+
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+//ruta asincrónica para cambiar status de usuario
+const setUsuarioStatus = async (estado, id) => {
+  try {
+    let params = {
+      text: "UPDATE skaters SET estado = $1 WHERE id = $2 RETURNING *",
+      values: [estado, id],
+    };
+    const result = await pool.query(params);
+    const usuario = result.rows[0];
+    return usuario;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+
+
+async function getUsuario(email, password) {
+  try {
+    let params = {
+      text: 'SELECT * FROM skaters WHERE email = $1',
+      values: [email]
+    }
+    const result = await pool.query(params);
+    if (result.rowCount > 0) {
+      const isSame = await bcrypt.compare(password, result.rows[0].password);
+      console.log("isSame: ", isSame);
+      if (isSame) {
+        return result.rows;
+      }
+      else {
+        return [];
+      }
+    }
+    else {
+      return result.rows;
+    }
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
+
+// ruta para actualizar datos de perfil
+const setDatosUsuario = async (
+  email,
+  nombre,
+  password,
+  anos_experiencia,
+  especialidad
+) => {
+  try {
+    let passwordHash = await hashPassword(password);
+    let params = {
+      text: `UPDATE skaters SET nombre = $1, password =$2, anos_experiencia = $3, especialidad = $4  WHERE email = $5 RETURNING *`,
+      values: [nombre, passwordHash, anos_experiencia, especialidad, email],
+    };
+    const result = await pool.query(params);
+
+    const usuario = result.rows;
+    return usuario;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+//ruta para eliminar usuario
+const deleteCuenta = async (email)=> {
+  try {
+    let params = {
+      text:'DELETE FROM skaters WHERE email = $1',
+      values: [email]
+    }
+    const result = await pool.query(params)
+    return result.rowCount;
+  } catch (e) {
+    console.log(e);
+  }
+  
+   
+}
+
+//hash para encriptar el password
+const hashPassword = async (password)=> {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
+
+
+module.exports = {
+  nuevoUsuario,
+  getUsuarios,
+  setUsuarioStatus,
+  getUsuario,
+  setDatosUsuario,
+  deleteCuenta,
+}
---
+++ index.js
@@ -0,0 +1,253 @@
+const express = require("express");
+const app = express();
+require('dotenv').config({ path: 'variables.env' });
+const exphbs = require("express-handlebars");
+const expressFileUpload = require("express-fileupload");
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.SECRET_KEY
+const PORT = process.env.PORT || 3000
+const host = process.env.HOST || '0.0.0.0'
+const path = require("path");
+const {
+  nuevoUsuario,
+  getUsuarios,
+  setUsuarioStatus,
+  getUsuario,
+  setDatosUsuario,
+  deleteCuenta,
+} = require("./consultas");
+
+app.listen(PORT, host, (req, res) => {
+  console.log(`Servidor ON port: ${PORT}`);
+});
+
+//Static
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
+
+//Midleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Middleware para express-fileupload
+app.use(
+  expressFileUpload({
+    limits: { fileSize: 5000000 },
+    abortOnLimit: true,
+    responseOnLimit:
+      "El peso del archivo que intentas subir supera el limite permitido",
+  })
+);
+
+// Importar motor de plantillas handlebars
+app.set("view engine", "hbs");
+
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: `${__dirname}/views/partials/`,
+    extname: ".hbs",
+    helpers: {
+      inc: function (value) {
+        return parseInt(value) + 1;
+      },
+    },
+  })
+);
+
+//Routes
+app.get("/", async (req, res) => {
+  try {
+    const usuarios = await getUsuarios();
+    res.render("home", { usuarios });
+  } catch (error) {
+    res.status(500).send({
+      error: `Algo salió mal... ${e}`,
+      code: 500,
+    });
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/registro", (req, res) => {
+  res.render("registro");
+});
+
+app.get("/admin", async (req, res) => {
+  try {
+    const usuarios = await getUsuarios();
+    res.render("admin", { usuarios });
+  } catch (e) {
+    res.status(500).send({
+      error: `Algo salió mal... ${e}`,
+      code: 500,
+    });
+  }
+});
+
+// Ruta para datos
+app.get("/datos", (req, res) => {
+  const { token } = req.query;
+  jwt.verify(token, secretKey, (err, decoded) => {
+    const { data } = decoded;
+    const email = data[0].email;
+    const nombre = data[0].nombre;
+    const password = data[0].password;
+    const anos_experiencia = data[0].anos_experiencia;
+    const especialidad = data[0].especialidad;
+    err
+      ? res.status(401).send({
+          error: "401 Unauthorized",
+          message: "Usted no está autorizado para estar aquí",
+          token_error: err.message,
+        })
+      : res.render("datos", {
+          email,
+          nombre,
+          password,
+          anos_experiencia,
+          especialidad,
+        });
+  });
+});
+
+//ruta para cambiar datos
+app.put("/datos", async (req, res) => {
+  const { email, nombre, password, anos_experiencia, especialidad } = req.body;
+  try {
+    const usuario = await setDatosUsuario(
+      email,
+      nombre,
+      password,
+      anos_experiencia,
+      especialidad
+    );
+    res.status(200).send(usuario);
+  } catch (e) {
+    res.status(500).send({
+      error: `Algo salió mal... ${e}`,
+      code: 500,
+    });
+  }
+});
+
+// Ruta POST /registro
+app.post("/registro", async (req, res) => {
+  const {
+    email,
+    nombre,
+    password,
+    password_2,
+    anos_experiencia,
+    especialidad,
+  } = req.body;
+  const { foto } = req.files;
+  const { name } = foto;
+  if (password !== password_2) {
+    res.send(
+      '<script>alert("Las contraseñas no coinciden."); window.location.href = "/registro"; </script>'
+    );
+  } else {
+    try {
+      const respuesta = await nuevoUsuario(
+        email,
+        nombre,
+        password,
+        anos_experiencia,
+        especialidad,
+        name
+      ).then(() => {
+        foto.mv(`${__dirname}/public/uploads/${name}`, (err) => {
+          res.send(
+            '<script>alert("Se ha registrado con éxito."); window.location.href = "/login"; </script>'
+          );
+        });
+      });
+    } catch (e) {
+      res.status(500).send({
+        error: `Algo salió mal... ${e}`,
+        code: 500,
+      });
+    }
+  }
+});
+
+// Ruta GET /usuarios
+app.get("/usuarios", async (req, res) => {
+  const respuesta = await getUsuarios();
+  res.send(respuesta);
+});
+
+// ruta para cambiar estado
+app.put("/usuarios", async (req, res) => {
+  const { estado, id } = req.body;
+  try {
+    const usuarios = await setUsuarioStatus(estado, id);
+    res.status(200).send(usuarios);
+  } catch (e) {
+    res.status(500).send({
+      error: `Algo salió mal... ${e}`,
+      code: 500,
+    });
+  }
+});
+
+//ruta para verificar que el email y password existen en la base de datos
+app.post("/verify", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await getUsuario(email, password);
+
+  if (email === "" || password === "") {
+    res.status(401).send({
+      error: "Debe completar todos los campos",
+      code: 401,
+    });
+  } else {
+    if (user.length != 0) {
+      if (user[0].estado === true) {
+        const token = jwt.sign(
+          {
+            exp: Math.floor(Date.now() / 1000) + 180,
+            data: user,
+          },
+          secretKey
+        );
+        res.send(token);
+        console.log(user[0].estado);
+      } else {
+        res.status(401).send({
+          error: `El registro de este usuario no ha sido aprobado`,
+          code: 401,
+        });
+        console.log(user[0].estado);
+      }
+    } else {
+      res.status(404).send({
+        error:
+          "Este usuario no está registrado en la base de datos o la contraseña es incorrecta.",
+        code: 404,
+      });
+    }
+  }
+});
+
+// ruta para eliminar los usuarios
+app.delete("/eliminar_cuenta/:email", async (req, res) => {
+  try {
+    const { email } = req.params;
+    const respuesta = await deleteCuenta(email);
+    res.status(200).send(respuesta.toString());
+  } catch (e) {
+    res.status(500).send({
+      error: `Algo salió mal... ${e}`,
+      code: 500,
+    });
+  }
+});
